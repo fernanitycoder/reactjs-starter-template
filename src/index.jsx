5f992743b5d99b8a6aef0cbc6a74915863c80864
fernanitycoder <5148837+fernanitycoder@users.noreply.github.com>
2018-05-19 18:01:29 -0500

    a header component was added to the App

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+
+class Header extends React.Component {
+    render() {
+        return (
+            <nav>
+                <ul className="navbar">
+                    <li className="nav-item"><a href="#">Home</a></li>
+                    <li className="nav-item"><a href="#">About</a></li>
+                    <li className="nav-item"><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+
 class App extends React.Component {
     render() {
         return (
-            <div>
+            <div className="container">
+                <Header />
                 <h2>This ReactJS project is now working</h2>
             </div>
         )
